a278877b46e3ed08da2c44403ca016d5b7d41686
Andric LibreSinn <1066744+mrpotatoes@users.noreply.github.com>
2025-05-17 12:37:43 -0400

    Adding image co-location

--- config/11ty/plugins/md.ts
+++ config/11ty/plugins/md.ts
@@ -1,6 +1,27 @@
 import markdownIt from 'markdown-it'
+import path from 'path'
+import markdownItEleventyImg from 'markdown-it-eleventy-img'
+import { outputDir, workingDir } from '@utils/variables'
 
 export default markdownIt({
   html: true,
   linkify: true
 })
+  .use(markdownItEleventyImg, {
+    imgOptions: {
+      widths: [1000, 800, 600],
+      urlPath: '/assets/imgs/',
+      outputDir: path.join(outputDir(), '/assets/imgs'),
+      // formats: ['avif', 'webp', 'jpeg'],
+    },
+    globalAttributes: {
+      class: 'markdown-image',
+      decoding: 'async',
+      sizes: '100vw',
+    },
+
+    resolvePath: (filepath: string, env: any) =>
+      (filepath.charAt(0) === '/')
+        ? path.join(workingDir(), 'config/', filepath)
+        : path.join(path.dirname(env.page.inputPath), filepath)
+  })
